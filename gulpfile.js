3ebb80120495aec6e96975a55956ae9545a68f43
MiguelCabailo <mcliguelcabailo12@gmail.com>
2018-07-18 18:19:24 +1000

    edited gulp and scss files

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var nodemon = require('gulp-nodemon');
 
-
 gulp.task('serve', ['watch'], function(){
     return nodemon({
       script: './app.js',
     })
     .on('restart', function(){
       console.log('restarted');
-    })
+    });
+    
 });
 
 // define a task
@@ -18,12 +18,13 @@ gulp.task('sass', function(){
     // source file
     return gulp.src('./public/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./public/css'))
 });
 
 // default task
 gulp.task('watch', function(){
-    gulp.watch('./public/scss/*.scss', ['sass'])
+    gulp.watch('./public/scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['serve', 'sass', 'watch']);
+gulp.task('default', ['serve', 'watch']);
+
